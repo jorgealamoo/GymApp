62ff354558d625d73264dec61c5f06164685d7d0
Jorge <jorge.alamo@ADEdelivered.com>
2024-11-12 17:01:50 +0000

    login muy avanzado

--- app/src/main/java/com/example/gymapp/ui/login/LoginView.kt
+++ app/src/main/java/com/example/gymapp/ui/login/LoginView.kt
@@ -2,7 +2,6 @@ package com.example.gymapp.ui.login
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,11 +12,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,20 +28,27 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.gymapp.R
 import com.example.gymapp.ui.theme.Black
+import com.example.gymapp.ui.theme.GymOrange
 import com.example.gymapp.ui.theme.GymRed
+import com.example.gymapp.ui.theme.GymYellow
 import com.example.gymapp.ui.theme.White
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Login(){
     var username by remember { mutableStateOf("") }
@@ -63,6 +72,7 @@ fun Login(){
             modifier = Modifier
                 .size(350.dp, 500.dp)
                 .padding(16.dp)
+                .clip(RoundedCornerShape(25.dp))
                 .background(GymRed.copy(alpha = 0.6f))
                 .align(Alignment.Center),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -79,27 +89,63 @@ fun Login(){
             OutlinedTextField(
                 value = username,
                 onValueChange = { username = it },
-                placeholder = { Text(stringResource(id = R.string.username), fontWeight = FontWeight.Bold) },
+                placeholder = { Text(stringResource(id = R.string.username), fontWeight = FontWeight.Bold, fontSize = 18.sp) },
+                textStyle = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold),
                 modifier = Modifier.width(265.dp),
                 shape = RoundedCornerShape(25.dp),
+                leadingIcon = {
+                    Icon(Icons.Filled.Person, contentDescription = null, tint = Color.Black)
+                },
                 colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Black,
-                    focusedPlaceholderColor = Black
+                    focusedContainerColor = White,
+                    focusedPlaceholderColor = White
                 )
             )
 
             Spacer(modifier = Modifier.height(16.dp))
 
             OutlinedTextField(
-                value = username,
-                onValueChange = { username = it },
-                placeholder = { Text(stringResource(id = R.string.password), fontWeight = FontWeight.Bold) },
+                value = password,
+                onValueChange = { password = it },
+                placeholder = { Text(stringResource(id = R.string.password), fontWeight = FontWeight.Bold, fontSize = 18.sp) },
+                textStyle = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold),
                 modifier = Modifier.width(265.dp),
                 shape = RoundedCornerShape(25.dp),
+                leadingIcon = {
+                    Icon(Icons.Filled.Lock, contentDescription = null, tint = Color.Black)
+                },
                 colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Black,
-                    focusedPlaceholderColor = Black
+                    focusedContainerColor = White,
+                    focusedPlaceholderColor = White
+                ),
+                visualTransformation = PasswordVisualTransformation()
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(
+                onClick = { println("Login done") },
+                modifier = Modifier
+                    .width(170.dp)
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(Black)
+                ) {
+                Text(
+                    text = stringResource(id = R.string.login),
+                    color = White,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+
                 )
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = stringResource(id = R.string.forgot_password),
+                color = Black,
+                fontSize = 16.sp,
+                textDecoration = TextDecoration.Underline
             )
         }
     }
@@ -109,5 +155,5 @@ fun Login(){
 @Composable
 @Preview
 fun LoginPreview(){
-    Login();
+    Login()
 }
