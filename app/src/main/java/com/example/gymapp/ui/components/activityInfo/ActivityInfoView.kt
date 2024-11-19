ead80e8a9884c43c9129da9373d3ec8331a1351c
Jorge Álamo Santana <123117518+jorgealamoo@users.noreply.github.com>
2024-11-19 17:19:43 +0000

    ActivityInfoView algo más avanzado

--- app/src/main/java/com/example/gymapp/ui/components/activityInfo/ActivityInfoView.kt
+++ app/src/main/java/com/example/gymapp/ui/components/activityInfo/ActivityInfoView.kt
@@ -18,11 +18,18 @@ import com.example.gymapp.ui.theme.Black
 import com.example.gymapp.ui.theme.GymOrange
 import com.example.gymapp.ui.theme.GymRed
 import com.example.gymapp.ui.theme.White
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 
 @Composable
 fun ActivityInfo(activityTitle: Int,
-                 gymCity: Int
+                 gymCity: Int,
+                 date: String,
+                 startTime: String,
+                 endTime: String,
+                 coach: String = "Coach Name",
+                 room: String
                  ){
     Column(
         modifier = Modifier
@@ -45,12 +52,39 @@ fun ActivityInfo(activityTitle: Int,
             modifier = Modifier.padding(25.dp, 5.dp, 0.dp, 0.dp)
         )
 
-        ScheduleAndCoach()
+        ScheduleAndCoach(parseDate(date), startTime, endTime, coach)
+
+        Text(
+            text = stringResource(R.string.room),
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            color = GymRed,
+            modifier = Modifier.padding(25.dp, 30.dp, 0.dp, 0.dp)
+        )
+
+        Text(
+            text = room,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium,
+            color = Black,
+            modifier = Modifier.padding(25.dp, 3.dp, 0.dp, 0.dp)
+        )
     }
 }
 
+fun parseDate(dateString: String): Date {
+    val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    return dateFormat.parse(dateString) ?: Date()
+}
+
 @Composable
-fun ScheduleAndCoach(date: String = "hola", startTime: String = "que", endTime: String = "tal"){
+fun ScheduleAndCoach(date: Date, startTime: String, endTime: String, coach: String){
+    val dateFormat = SimpleDateFormat("EEEE, dd/MM", Locale.getDefault())
+    val formattedDate = dateFormat.format(date)
+    val dayWithCapitalLetter = formattedDate.replaceFirstChar {
+        if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
+    }
+
     Row(
         modifier = Modifier
             .padding(25.dp, 40.dp, 0.dp, 0.dp)
@@ -62,8 +96,34 @@ fun ScheduleAndCoach(date: String = "hola", startTime: String = "que", endTime: 
                 fontWeight = FontWeight.Bold,
                 color = GymRed
             )
-            
 
+            Text(
+                text = "$dayWithCapitalLetter\n$startTime - $endTime",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Medium,
+                color = Black,
+                modifier = Modifier.padding(top = 3.dp)
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(start = 90.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.coach),
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                color = GymRed
+            )
+
+            Text(
+                text = coach,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Medium,
+                color = Black,
+                modifier = Modifier.padding(top = 3.dp)
+            )
         }
     }
 }
@@ -72,6 +132,11 @@ fun ScheduleAndCoach(date: String = "hola", startTime: String = "que", endTime: 
 @Preview
 fun ActivityInfoPreview(){
     ActivityInfo(activityTitle = R.string.loren_ipsum,
-        gymCity = R.string.lpgc_u_know
+        gymCity = R.string.lpgc_u_know,
+        date = "21/11/2024",
+        startTime = "00:00",
+        endTime = "00:00",
+        coach = "Manolo Rodríguez",
+        room = "Studio 1"
     )
 }
