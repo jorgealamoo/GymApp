4f84f64db8ddf7bbaa1b0484c8804b82f8358b31
Jorge <jorge.alamo@ADEdelivered.com>
2024-11-22 16:49:36 +0000

    Activity sin hacer

---
+++ app/src/main/java/com/example/gymapp/ui/components/exercise/ExerciseView.kt
@@ -0,0 +1,15 @@
+package com.example.gymapp.ui.components.exercise
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Activity(){
+
+}
+
+@Composable
+@Preview
+fun ActivityPreview(){
+    Activity()
+}
