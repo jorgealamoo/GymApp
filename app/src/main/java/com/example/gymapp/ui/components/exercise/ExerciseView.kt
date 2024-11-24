102f38241110bb8bde44231a6254838480938f7f
Jorge Álamo Santana <123117518+jorgealamoo@users.noreply.github.com>
2024-11-24 17:55:42 +0000

    Exercise sin nada

--- app/src/main/java/com/example/gymapp/ui/components/exercise/ExerciseView.kt
+++ app/src/main/java/com/example/gymapp/ui/components/exercise/ExerciseView.kt
@@ -1,15 +1,26 @@
 package com.example.gymapp.ui.components.exercise
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun Activity(){
-
+fun Exercise(exerciseName: String){
+    Row(
+        modifier = Modifier
+            .size(320.dp, 90.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        "imagen"
+    }
 }
 
 @Composable
 @Preview
-fun ActivityPreview(){
-    Activity()
+fun ExercisePreview(){
+    Exercise("Loren ipsum")
 }
