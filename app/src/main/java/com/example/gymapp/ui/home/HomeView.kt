d131ff396e09beccc9b406a1f8f4447c57871dac
Pedro Manuel <pedrom.rodriguezhernandez@gmail.com>
2024-11-26 17:04:14 +0000

    Fleje cosas en verdad

--- app/src/main/java/com/example/gymapp/ui/home/HomeView.kt
+++ app/src/main/java/com/example/gymapp/ui/home/HomeView.kt
@@ -2,10 +2,10 @@ package com.example.gymapp.ui.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -13,11 +13,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,7 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -40,12 +37,9 @@ import com.example.gymapp.ui.theme.GymRed
 import com.example.gymapp.ui.theme.White
 
 @Composable
-fun Login(){
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-
+fun Home(modifier: Modifier = Modifier){
     Box(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
             .background(White)
     ) {
@@ -55,52 +49,25 @@ fun Login(){
             contentScale = ContentScale.FillBounds,
             modifier = Modifier
                 .matchParentSize()
-                .graphicsLayer(alpha = 0.6f)
+                .graphicsLayer(alpha = 0.6f),
         )
 
         Column(
             modifier = Modifier
-                .size(350.dp, 500.dp)
-                .padding(16.dp)
-                .background(GymRed.copy(alpha = 0.6f))
-                .align(Alignment.Center),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.gymapplogo),
-                contentDescription = R.string.app_logo.toString(),
-                modifier = Modifier.size(140.dp)
-            )
+            Spacer(modifier = Modifier.height(25.dp))
+            Row(
+                modifier = Modifier
+                    .width(312.dp)
+                    .height(65.dp)
+                    .clip(RoundedCornerShape(14.dp))
+                    .background(color = White.copy(alpha = 0.60f)),
+            ) {  }
 
-            Spacer(modifier = Modifier.height(40.dp))
-
-            OutlinedTextField(
-                value = username,
-                onValueChange = { username = it },
-                placeholder = { Text(stringResource(id = R.string.username), fontWeight = FontWeight.Bold) },
-                modifier = Modifier.width(265.dp),
-                shape = RoundedCornerShape(25.dp),
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Black,
-                    focusedPlaceholderColor = Black
-                )
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            OutlinedTextField(
-                value = username,
-                onValueChange = { username = it },
-                placeholder = { Text(stringResource(id = R.string.password), fontWeight = FontWeight.Bold) },
-                modifier = Modifier.width(265.dp),
-                shape = RoundedCornerShape(25.dp),
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Black,
-                    focusedPlaceholderColor = Black
-                )
-            )
         }
+
     }
 
 }
@@ -108,5 +75,5 @@ fun Login(){
 @Composable
 @Preview
 fun LoginPreview(){
-    Login();
+    Home();
 }
