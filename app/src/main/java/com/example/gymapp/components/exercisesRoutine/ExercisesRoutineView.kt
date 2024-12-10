50b01c960d6146bb62048715a755c6f106aee6c8
Jorge Álamo Santana <123117518+jorgealamoo@users.noreply.github.com>
2024-12-10 00:45:29 +0000

    Lo conseguí :_)

--- app/src/main/java/com/example/gymapp/components/exercisesRoutine/ExercisesRoutineView.kt
+++ app/src/main/java/com/example/gymapp/components/exercisesRoutine/ExercisesRoutineView.kt
@@ -119,161 +119,124 @@ fun ExerciseRoutineView(
             Text(text = errorMessage ?: "Unknown error", fontSize = 18.sp, fontWeight = FontWeight.Bold)
         }
     } else {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.Start
-        ) {
-            Text(
-                text = "Exercises for $routineName:",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(White)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.background),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .matchParentSize()
+                        .graphicsLayer(alpha = 0.6f)
+                )
 
-            exercisesList.forEach { exercise ->
-                exercise.forEach { (exerciseName, details) ->
-                    Text(
-                        text = "Exercise: $exerciseName",
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Medium,
-                        modifier = Modifier.padding(vertical = 4.dp)
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(220.dp)
+                ) {
+                    Image(
+                        painter = painterResource(routine.routineImage),
+                        contentDescription = stringResource(routine.routineString),
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize().graphicsLayer(alpha = 0.7f)
                     )
-                    Log.d("FirebaseData", "Details: $details")
-                    details.forEach { (key, value) ->
-                        Log.d("FirebaseData", "Exercise: $exerciseName, Key: $key, Value: $value")
-                        Text(
-                            text = "${key.toString()}: ${value.toString()}",
-                            fontSize = 14.sp,
-                            fontWeight = FontWeight.Light,
-                            modifier = Modifier.padding(start = 16.dp, bottom = 4.dp)
-                        )
-                    }
 
+                    Column(
+                        modifier = Modifier
+                            .padding(start = 20.dp, top = 140.dp)
+                    ) {
+                        Text(
+                            text = stringResource(routine.routineString),
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = White
+                        )
+
+                        Text(
+                            text = stringResource(R.string.day) + " $day",
+                            fontSize = 40.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = GymRed
+                        )
+
+                    }
+                }
+
+                Column(
+                    modifier = Modifier
+                        .padding(top = 220.dp)
+                        .fillMaxWidth()
+                        .background(White.copy(0.6f))
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    Spacer(modifier = Modifier.height(20.dp))
+
+                    Text(
+                        text = exercisesList.size.toString() + " " + stringResource(R.string.exercises),
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Black,
+                        modifier = Modifier.padding(start = 20.dp, bottom = 5.dp)
+                    )
+
+                    exercisesList.forEach { exercise ->
+                        exercise.forEach { (exerciseName, details) ->
+                            var repetitionsNumber: Int = 0
+                            var weightNumber: Int = 0
+                            details.forEach { (key, value) ->
+                                if (key == "repetitions") repetitionsNumber = value
+                                if (key == "weight") weightNumber = value
+                            }
+                            Exercise(
+                                exerciseName = exerciseName,
+                                repetitions = repetitionsNumber,
+                                weight = weightNumber,
+                                modifier = Modifier.padding(start = 20.dp, top = 10.dp, bottom = 10.dp)
+                            )
+                        }
+                    }
+                }
+
+                IconButton(
+                    onClick = {
+                        navController.navigate(AppScreens.RoutineTableScreen.route)
+                    },
+                    modifier = Modifier
+                        .align(Alignment.TopStart)
+                        .padding(10.dp)
+                        .clip(RoundedCornerShape(15.dp))
+                        .background(White.copy(0.7f))
+                        .size(40.dp)
+                ) {
+                    Icon(
+                        imageVector =  Icons.Default.ArrowBack,
+                        contentDescription = stringResource(R.string.return_button),
+                        tint = GymRed,
+                        modifier = Modifier.size(50.dp)
+                    )
+                }
+
+                Button(
+                    onClick = { println("Completed") },
+                    colors = ButtonDefaults.buttonColors(containerColor = GymRed),
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(80.dp)
+                        .align(Alignment.BottomCenter)
+                        .padding(10.dp, 0.dp, 10.dp, 20.dp)
+                ) {
+                    Text(
+                        text = stringResource(R.string.complete),
+                        color = White,
+                        fontSize = 32.sp,
+                        fontWeight = FontWeight.Bold
+                    )
                 }
             }
-        }
     }
-
-/*
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(White)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.background),
-            contentDescription = null,
-            contentScale = ContentScale.FillBounds,
-            modifier = Modifier
-                .matchParentSize()
-                .graphicsLayer(alpha = 0.6f)
-        )
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(220.dp)
-        ) {
-            Image(
-                painter = painterResource(routine.routineImage),
-                contentDescription = stringResource(routine.routineString),
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize().graphicsLayer(alpha = 0.7f)
-            )
-
-            Column(
-                modifier = Modifier
-                    .padding(start = 20.dp, top = 140.dp)
-            ) {
-                Text(
-                    text = stringResource(routine.routineString),
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = White
-                )
-
-                Text(
-                    text = stringResource(R.string.day) + " $day",
-                    fontSize = 40.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = GymRed
-                )
-
-            }
-        }
-
-        Column(
-            modifier = Modifier
-                .padding(top = 220.dp)
-                .fillMaxWidth()
-                .background(White.copy(0.6f))
-                .verticalScroll(rememberScrollState())
-        ) {
-            Spacer(modifier = Modifier.height(20.dp))
-
-            Text(
-                text = exercises.size.toString() + " " + stringResource(R.string.exercises),
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold,
-                color = Black,
-                modifier = Modifier.padding(start = 20.dp, bottom = 5.dp)
-            )
-
-            exercises.forEach { exerciseName ->
-                Exercise(
-                    exerciseName = exerciseName,
-                    repetitions = 12,
-                    weight = 90,
-                    modifier = Modifier.padding(start = 20.dp, top = 10.dp, bottom = 10.dp)
-                )
-
-                Spacer(modifier = Modifier.height(1.dp)
-                    .background(Black)
-                    .width(305.dp)
-                    .align(Alignment.CenterHorizontally)
-                )
-            }
-        }
-
-        IconButton(
-            onClick = {
-                navController.navigate(AppScreens.RoutineTableScreen.route)
-            },
-            modifier = Modifier
-                .align(Alignment.TopStart)
-                .padding(10.dp)
-                .clip(RoundedCornerShape(15.dp))
-                .background(White.copy(0.7f))
-                .size(40.dp)
-        ) {
-            Icon(
-                imageVector =  Icons.Default.ArrowBack,
-                contentDescription = stringResource(R.string.return_button),
-                tint = GymRed,
-                modifier = Modifier.size(50.dp)
-            )
-        }
-
-        Button(
-            onClick = { println("Completed") },
-            colors = ButtonDefaults.buttonColors(containerColor = GymRed),
-            shape = RoundedCornerShape(10.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(80.dp)
-                .align(Alignment.BottomCenter)
-                .padding(10.dp, 0.dp, 10.dp, 20.dp)
-        ) {
-            Text(
-                text = stringResource(R.string.complete),
-                color = White,
-                fontSize = 32.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
-    }
-
- */
 }
