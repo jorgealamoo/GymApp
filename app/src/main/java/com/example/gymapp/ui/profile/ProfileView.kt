3afc26f523568fdb00f71ea7f4efed9d4799919c
Jorge <jorge.alamo@ADEdelivered.com>
2024-12-13 16:05:40 +0000

    mini cambios 2

--- app/src/main/java/com/example/gymapp/ui/profile/ProfileView.kt
+++ app/src/main/java/com/example/gymapp/ui/profile/ProfileView.kt
@@ -2,6 +2,7 @@ package com.example.gymapp.ui.profile
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -127,6 +128,7 @@ fun ContentProfile(
             Spacer(modifier = Modifier.height(25.dp))
 
             Text(
+                modifier = Modifier.clickable { /* Te lleva a una página web imaginaria */ },
                 text = stringResource(R.string.change_password),
                 fontSize = 22.sp,
                 textDecoration = TextDecoration.Underline
